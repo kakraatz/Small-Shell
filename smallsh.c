31a3398de4b06c2b4fd61e8ddc55da16994c902a
Kevin Kraatz <kraatzk@os1.engr.oregonstate.edu>
2022-11-02 02:42:52 -0700

    idk

---
+++ smallsh.c
@@ -0,0 +1,20 @@
+/* Author: Kevin Kraatz 
+ * CS344 Fall 2022
+ * Assignment 3 - smallsh */
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+
+main
+
+builtin_commands
+
+other_commands
